76330e706db9d02419675fdd9bd1296956366e46
Ruben-am <ru.alba.dev@gmail.com>
2025-10-30 09:03:50 +0100

    feat: add forgot password screen

--- app/src/main/java/com/rubenalba/placement/MainActivity.java
+++ app/src/main/java/com/rubenalba/placement/MainActivity.java
@@ -1,6 +1,9 @@
 package com.rubenalba.placement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvForgotPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        initComponents();
+        initListeners();
+    }
+
+    private void initComponents() {
+        tvForgotPassword = findViewById(R.id.tv_main_forgotpassword);
+    }
+
+    private void initListeners() {
+        tvForgotPassword.setOnClickListener(View -> {
+            Intent intent = new Intent(MainActivity.this, ForgotPasswordActivity.class);
+
+            startActivity(intent);
         });
     }
 }
